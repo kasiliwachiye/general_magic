4c9364860e9f186515e788bdb6a1dbc9f64c7951
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-09-21 08:04:26 +0300

    chore: implement navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
 export default App;
+
