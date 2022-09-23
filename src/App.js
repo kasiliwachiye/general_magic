5f02f47ac1f10d4b140596520562c2421b5414ac
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-09-23 15:58:05 +0300

    chore: refactor code, fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
-import Home from "./components/Home";
+import Home from "./pages/Home";
+import Liked from "./pages/Liked";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/liked' element={<Liked />} />
+        </Routes>
+      </Router>
     </>
   );
 }
